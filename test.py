b5dab5b30b9bd3ff5e254b7ab9486cf9ac08d1c4
Alban Chauvin <achauvin@centumadetel.com>
2017-11-01 13:52:41 -0400

    Formatting using a function to get the list of games pages links

--- test.py
+++ test.py
@@ -1,29 +1,30 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-site = "https://www.basketball-reference.com"
+gSite = "https://www.basketball-reference.com"
+gSeasonPage = gSite + "/leagues/NBA_2017_games.html"
 
-seasonPage = site + "/leagues/NBA_2017_games.html"
+def GetScheduleGamesPages(BRSeasonWebPage):
+	results = []
 
-req = urllib2.Request(seasonPage)
-resp = urllib2.urlopen(req)
-page = resp.read()
+	req = urllib2.Request(BRSeasonWebPage)
+	resp = urllib2.urlopen(req)
+	page = resp.read()
 
-soup = BeautifulSoup(page)
-schedule_pages = soup.find_all("div", class_="filter")
+	soup = BeautifulSoup(page)
+	schedule_pages = soup.find_all("div", class_="filter")
 
-links = schedule_pages[0].find_all("a")
+	links = schedule_pages[0].find_all("a")
 
-for link in links:
-	scheduleLink = site + link["href"]
-	print("***")
-	print(scheduleLink+"\n")
-	schedulePage = urllib2.urlopen(scheduleLink)
-	soup2 = BeautifulSoup(schedulePage)
-	gamePages = soup2.find_all("table", {"id":"schedule"})
-	
-	for temp in gamePages[0].find_all("a"):
-		if temp.string == "Box Score":
-			print(site + temp["href"])
-	print("\n")
+	for link in links:
+		scheduleLink = gSite + link["href"]
+		schedulePage = urllib2.urlopen(scheduleLink)
+		soup2 = BeautifulSoup(schedulePage)
+		gamePages = soup2.find_all("table", {"id":"schedule"})
+		for temp in gamePages[0].find_all("a"):
+			if temp.string == "Box Score":
+				results.append(temp["href"])
+	return results;
+
+print(GetScheduleGamesPages(gSeasonPage))
 
