7b00723307ff4c246ae25a6b8ec76298f4e05321
Alban Chauvin <achauvin@centumadetel.com>
2017-11-01 15:57:44 -0400

    Database creation modified

--- DataStorage/dbCreator.py
+++ DataStorage/dbCreator.py
@@ -3,31 +3,41 @@ import sqlite3
 conn = sqlite3.connect('data.db')
 
 #Create players
-conn.exectute('''CREATE TABLE players (
+conn.execute('''CREATE TABLE players (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT)''')
 
 #Create teams
-conn.exectute('''CREATE TABLE teams (
+conn.execute('''CREATE TABLE teams (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		name TEXT
 		city TEXT)''')
 
 #Create game_records
-conn.exectute('''CREATE TABLE game_records (
+conn.execute('''CREATE TABLE game_records (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		player INTEGER,
 		team INTEGER,
 		SP INTEGER,
 		FG INTEGER,
 		FGA INTEGER,
-		
+		THREEP INTEGER,
+		THREEPA INTEGER,
+		FT INTEGER,
+		FTA INTEGER,
+		ORB INTEGER,
+		DRB INTEGER,
+		AST INTEGER,
+		STL INTEGER,
+		BLK INTEGER,
+		TOV INTEGER,
+		PF INTEGER,
+		plusminus INTEGER,
 		FOREIGN KEY(player) REFERENCES players(id),
-		FOREIGN KEY(team) REFERENCES teams(id),
-		)''')
+		FOREIGN KEY(team) REFERENCES teams(id))''')
 
 #Create games
-conn.exectute('''CREATE TABLE games (
+conn.execute('''CREATE TABLE games (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		date TEXT,
 		home_team INTEGER,
@@ -35,7 +45,6 @@ conn.exectute('''CREATE TABLE games (
 		game_record INTEGER,
 		FOREIGN KEY(home_team) REFERENCES teams(id),
 		FOREIGN KEY(visitor_team) REFERENCES teams(id),
-		FOREIGN KEY(game_record) REFERENCES game_records(id),
-		)''')
+		FOREIGN KEY(game_record) REFERENCES game_records(id))''')
 
 conn.close()
