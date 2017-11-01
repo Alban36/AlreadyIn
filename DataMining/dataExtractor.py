7632fc184069027cc8fa02180bebdc7045595f10
Alban Chauvin <achauvin@centumadetel.com>
2017-11-01 19:04:14 -0400

    Extraction of game date and opponents adde

--- DataMining/dataExtractor.py
+++ DataMining/dataExtractor.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib2
 import sqlite3
+import re
+from datetime import datetime
 
 gSite = "https://www.basketball-reference.com"
 gSeasonPage = gSite + "/leagues/NBA_2017_games.html"
@@ -8,7 +10,11 @@ gTeamsPage = gSite + "/teams/"
 
 gDB = "../DataStorage/db/data.db"
 
+gExtractTeams = False;
+
+#Retrieve the web pages containing the games records of the entire season schedule
 def GetScheduleGamesPages(BRSeasonWebPage):
+	print("Extracting games pages links ...")
 	results = []
 
 	req = urllib2.Request(BRSeasonWebPage)
@@ -27,9 +33,11 @@ def GetScheduleGamesPages(BRSeasonWebPage):
 		gamePages = soup2.find_all("table", {"id":"schedule"})
 		for temp in gamePages[0].find_all("a"):
 			if temp.string == "Box Score":
-				results.append(temp["href"])
+				results.append(gSite + temp["href"])
+	print("DONE.")
 	return results;
 
+#Insert a new team into the DB
 def InsertTeam(teamName):
 	inserted = False
 	conn = sqlite3.connect(gDB)
@@ -43,6 +51,7 @@ def InsertTeam(teamName):
 	conn.close()
 	return inserted;
 
+#Read the list of the team from the teams page of BR and insert them if necessary into the DB
 def ExtractTeams(BRTeamsWebPage):
 	req = urllib2.Request(BRTeamsWebPage)
 	resp = urllib2.urlopen(req)
@@ -59,17 +68,85 @@ def ExtractTeams(BRTeamsWebPage):
 			print("Team '"+team.find_all("a")[0].string+"' was NOT inserted to the database. It is already inside it.")
 	return;
 
+#Convert a word date to date type
+def StrToDate(value):
+	tokens = re.findall(r"[\w']+",value)
+
+	month = tokens[0];
+	if month.lower() == "january":
+		month = "01"
+	elif month.lower() == "february": 
+		month = "02"
+	elif month.lower() == "march": 
+		month = "03"
+	elif month.lower() == "april": 
+		month = "04"
+	elif month.lower() == "may": 
+		month = "05"
+	elif month.lower() == "june": 
+		month = "06"
+	elif month.lower() == "july": 
+		month = "07"
+	elif month.lower() == "august": 
+		month = "08"
+	elif month.lower() == "september": 
+		month = "09"
+	elif month.lower() == "october": 
+		month = "10"
+	elif month.lower() == "november": 
+		month = "11"
+	elif month.lower() == "december": 
+		month = "12"
+
+	return datetime.strptime(tokens[2]+' '+tokens[1]+' '+month,'%Y %d %m');
+
+#Extract the games data and games records and store them in the db
 def ExtractGameRecord(BRGameWebPage):
-	req = urllib2.Request(BRSeasonWebPage)
+	awayTeam = ""
+	homeTeam = ""
+	date = ""
+
+	print("Extracting game record for page "+BRGameWebPage+" ...")
+	req = urllib2.Request(BRGameWebPage)
 	resp = urllib2.urlopen(req)
 	page = resp.read()
 
+	if not page:
+		print("Error : webpage "+BRGameWebPage+" is empty. Data cannot be extracted.")	
+		return;
+
 	soup = BeautifulSoup(page)
 
-	
-	
+	description = soup.find_all("meta", {"name":"Description"})
+	if description != None:
+		#here extract from description the home team, the away team and the date
+		print(description[0]["content"])
+		text = description[0]["content"]
+		fp = text.find(" (");
+		vs = text.find("vs. ");
+		lp = text.rfind(" (");
+		lt = text.rfind("- ");	
+
+		awayTeam = text[12:fp]
+		homeTeam = text[vs+4:lp]
+		date = text[lt+2:]
+
+		print(awayTeam)
+		print(homeTeam)	
+		print(date)
+	else:
+		print("Error: could not find game description")
+	print("DONE.")
 	return;
 
-ExtractTeams(gTeamsPage)
-#print(GetScheduleGamesPages(gSeasonPage))
+#Extracting teams if necessary (it shoudln't once done once)
+if gExtractTeams==True:
+	ExtractTeams(gTeamsPage)
+
+date = StrToDate("October 1, 2016")
+
+print(date)
+#games_pages_list = GetScheduleGamesPages(gSeasonPage)
+#for game_page in games_pages_list:
+#	ExtractGameRecord(game_page)
 
